d3d7646d6c7dbb839d0f54deb6f0222b71b9991e
Dimitar Georgiev <eng.d.georgiev@gmail.com>
2024-07-22 17:31:39 +0300

    Added structure and base functionality to the bank_app_task

---
+++ OOP_Exams/exam_05_08_2023/bank_app_task/project/clients/adult.py
@@ -0,0 +1,12 @@
+from project.clients.base_client import BaseClient
+
+
+class Adult(BaseClient):
+    INITIAL_INTEREST_RATE = 4.0
+    INCREASE_CLIENT_INTEREST = 2.0
+
+    def __init__(self, name: str, client_id: str, income: float):
+        super().__init__(name, client_id, income, self.INITIAL_INTEREST_RATE)
+
+    def increase_clients_interest(self):
+        self.interest += self.INCREASE_CLIENT_INTEREST
