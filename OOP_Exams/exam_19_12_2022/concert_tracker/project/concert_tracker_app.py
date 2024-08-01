a270f33858d79139f191d1bc37077cc8c1825453
Dimitar Georgiev <eng.d.georgiev@gmail.com>
2024-08-01 14:03:01 +0300

    Completed the functionality for task: concert tracker

--- OOP_Exams/exam_19_12_2022/concert_tracker/project/concert_tracker_app.py
+++ OOP_Exams/exam_19_12_2022/concert_tracker/project/concert_tracker_app.py
@@ -16,19 +16,103 @@ class ConcertTrackerApp:
         self.concerts: List[Concert] = []
 
     def create_musician(self, musician_type: str, name: str, age: int):
-        pass
+        musician = self.VALID_MUSICIANS.get(musician_type)
+
+        if musician is None:
+            raise ValueError("Invalid musician type!")
+
+        name_duplicating = next((m for m in self.musicians if m.name == name), None)
+        if name_duplicating:
+            raise Exception(f"{name} is already a musician!")
+
+        musician = self.VALID_MUSICIANS[musician_type](name, age)
+        self.musicians.append(musician)
+        return f"{name} is now a {musician_type}."
 
     def create_band(self, name: str):
-        pass
+        name_duplicating = next((b for b in self.bands if b.name == name), None)
+        if name_duplicating:
+            raise Exception(f"{name} band is already created!")
+
+        band = Band(name)
+        self.bands.append(band)
+        return f"{name} was created."
 
     def create_concert(self, genre: str, audience: int, ticket_price: float, expenses: float, place: str):
-        pass
+        is_place_taken = next((c for c in self.concerts if c.place == place), None)
+        if is_place_taken:
+            raise Exception(f"{is_place_taken.place} is already registered for {is_place_taken.genre} concert!")
+
+        new_concert = Concert(genre, audience, ticket_price, expenses, place)
+        self.concerts.append(new_concert)
+        return f"{genre} concert in {place} was added."
 
     def add_musician_to_band(self, musician_name: str, band_name: str):
-        pass
+        musician = next((m for m in self.musicians if m.name == musician_name), None)
+        if musician is None:
+            raise Exception(f"{musician_name} isn't a musician!")
+
+        band = next((b for b in self.bands if b.name == band_name), None)
+        if band is None:
+            raise Exception(f"{band_name} isn't a band!")
+
+        band.members.append(musician)
+        return f"{musician_name} was added to {band_name}."
 
     def remove_musician_from_band(self, musician_name: str, band_name: str):
-        pass
+        band = next((b for b in self.bands if b.name == band_name), None)
+        if band is None:
+            raise Exception(f"{band_name} isn't a band!")
+
+        musician = next((m for m in band.members if m.name == musician_name), None)
+        if musician is None:
+            raise Exception(f"{musician_name} isn't a member of {band_name}!")
+
+        band.members.remove(musician)
+        return f"{musician_name} was removed from {band_name}."
 
     def start_concert(self, concert_place: str, band_name: str):
-        pass
+        concert = next(c for c in self.concerts if c.place == concert_place)
+        band = next(b for b in self.bands if b.name == band_name)
+
+        singer = next((s for s in band.members if isinstance(s, Singer)), None)
+        drummer = next((d for d in band.members if isinstance(d, Drummer)), None)
+        guitarist = next((g for g in band.members if isinstance(g, Guitarist)), None)
+
+        if singer is None or drummer is None or guitarist is None:
+            raise Exception(f"{band_name} can't start the concert because it doesn't have enough members!")
+
+        if concert.genre == 'Rock':
+            for band_member in band.members:
+                if band_member.__class__.__name__ == 'Drummer' and \
+                        "play the drums with drumsticks" not in band_member.skills:
+                    raise Exception(f"The {band_name} band is not ready to play at the concert!")
+                if band_member.__class__.__name__ == 'Singer' and "sing high pitch notes" not in band_member.skills:
+                    raise Exception(f"The {band_name} band is not ready to play at the concert!")
+                if band_member.__class__.__name__ == 'Guitarist' and "play rock" not in band_member.skills:
+                    raise Exception(f"The {band_name} band is not ready to play at the concert!")
+
+        elif concert.genre == 'Metal':
+            for band_member in band.members:
+                if (band_member.__class__.__name__ == 'Drummer' and
+                        "play the drums with drumsticks" not in band_member.skills):
+                    raise Exception(f"The {band_name} band is not ready to play at the concert!")
+                if band_member.__class__.__name__ == 'Singer' and "sing low pitch notes" not in band_member.skills:
+                    raise Exception(f"The {band_name} band is not ready to play at the concert!")
+                if band_member.__class__.__name__ == 'Guitarist' and "play metal" not in band_member.skills:
+                    raise Exception(f"The {band_name} band is not ready to play at the concert!")
+
+        elif concert.genre == 'Jazz':
+            for band_member in band.members:
+                if band_member.__class__.__name__ == 'Drummer' \
+                        and "play the drums with drum brushes" not in band_member.skills:
+                    raise Exception(f"The {band_name} band is not ready to play at the concert!")
+                if band_member.__class__.__name__ == 'Singer' \
+                        and ("sing low pitch notes" not in band_member.skills
+                             or "sing high pitch notes" not in band_member.skills):
+                    raise Exception(f"The {band_name} band is not ready to play at the concert!")
+                if band_member.__class__.__name__ == 'Guitarist' and "play jazz" not in band_member.skills:
+                    raise Exception(f"The {band_name} band is not ready to play at the concert!")
+
+        profit = (concert.audience * concert.ticket_price) - concert.expenses
+        return f"{band_name} gained {profit:.2f}$ from the {concert.genre} concert in {concert_place}."
