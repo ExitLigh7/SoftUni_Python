bdfac3470e5131868c7bcdf3ed2a72aa2c14b19e
Dimitar Georgiev <eng.d.georgiev@gmail.com>
2024-08-02 17:24:17 +0300

    Completed the structure and base functionality for task: food_order

---
+++ OOP_Exams/exam_22_08_2022/food_order/project/client.py
@@ -0,0 +1,19 @@
+from typing import List
+from project.meals.meal import Meal
+
+
+class Client:
+    def __init__(self, phone_number: str):
+        self.phone_number = phone_number
+        self.shopping_cart: List[Meal] = []
+        self.bill: float = 0.0
+
+    @property
+    def phone_number(self):
+        return self.__phone_number
+
+    @phone_number.setter
+    def phone_number(self, value):
+        if not (value.startswith("0") and value.isdigit() and len(value) == 10):
+            raise ValueError("Invalid phone number!")
+        self.__phone_number = value
